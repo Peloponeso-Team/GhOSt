bcd8b77799716c288ac1dad6ee5c20af2b527e21
JuniorMario <myacount221100@gmail.com>
2017-01-13 17:50:05 -0200

    test file

--- GoAtSchool/main.go
+++ GoAtSchool/main.go
@@ -8,6 +8,15 @@ import (
 
 func main() {
 	fmt.Println(conv.Ml_to_mt(45))
-	fmt.Println(conv.Mt_to_ml(15000))
+	fmt.Println(conv.S_to_hr(15000))
+	fmt.Println(conv.Hr_to_s(2))
+	fmt.Println(conv.D_to_s(1))
+	fmt.Println(conv.S_to_d(455555))
+	fmt.Println(conv.Min_to_d(5000))
+	fmt.Println(conv.D_to_min(1))
+	fmt.Println(conv.Mlh_to_kmh(10))
+	fmt.Println(conv.Kmh_to_mlh(10))
+	fmt.Println(conv.Kmh_to_ms(10))
+	fmt.Println(conv.Ms_to_kmh(10))
 	GoAtSchool.Version()
 }
